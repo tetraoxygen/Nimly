c3e46759a85573ca84ff1c42e9046616a0aa2bcf
Charlie Welsh <tetraoxygen@icloud.com>
2018-11-02 20:40:16 +0000

    Initial commit. Only Village Fool mode works.
    Added all of the files, including Emacs crap.

---
+++ CMakeFiles/Nimly.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nimly.dir/nim.cpp.o"
+  "Nimly.pdb"
+  "Nimly"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nimly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
